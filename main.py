f6952df587d471e9325093df8d9aa87c6d92a3c2
lozanno00 <luis.lozano.sanz43@gmail.com>
2025-09-16 10:36:03 +0200

    commit luis

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     if "streamlit" not in sys.modules:
         print("Este dashboard debe ejecutarse con: streamlit run main.py")
     else:
-        dashboard.run()
+        dashboard.run()  
